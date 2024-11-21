523df495ccf951fc4e8b246ed3674e83b31d52c5
krustaceany <johanan@riskharbor.com>
2024-11-21 16:09:47 +0530

    refactor(cli): add prompter for confirmation

--- crates/cli/src/prompter.rs
+++ crates/cli/src/prompter.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 use color_eyre::eyre;
 use color_eyre::owo_colors::OwoColorize;
-use dialoguer::{theme::ColorfulTheme, Input, MultiSelect, Password, Select};
+use dialoguer::{theme::ColorfulTheme, Confirm, Input, MultiSelect, Password, Select};
 use strum::VariantNames;
 
 pub fn select_enum<T: VariantNames + ToString>(
@@ -94,4 +94,14 @@ fn find_default_index<T: ToString>(default: Option<&T>, variants: &[&str]) -> us
                 .position(|&variant| variant == d.to_string())
         })
         .unwrap_or(0)
+}
+
+pub fn confirm<T: Into<String>>(prompt: T, default: Option<bool>) -> eyre::Result<bool> {
+    let theme = ColorfulTheme::default();
+    let confirm = Confirm::with_theme(&theme).with_prompt(prompt);
+    let confirm = match default {
+        Some(default) => confirm.default(default),
+        None => confirm,
+    };
+    Ok(confirm.interact()?)
 }
